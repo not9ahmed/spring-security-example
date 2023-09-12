9faf314ae38f97fc159de51694792867ff75ba84
Ahmed Al-Thawadi <ahmed9althawadi@gmail.com>
2023-09-12 21:48:47 +0300

    first commit

---
+++ src/test/java/com/notahmed/springsecurityexample/SpringSecurityExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notahmed.springsecurityexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
